a7d2317c92589c29b5458f75a49b460d72fdd9cd
ssivadas <sibidas_sivadas@intuit.com>
2021-10-23 01:35:49 +0530

    bug fix - set json serialize issue

--- src/main/issue_fetcher.py
+++ src/main/issue_fetcher.py
@@ -43,7 +43,7 @@ def handler(event, context):
         log.info("Invoke response: {}".format(json.dumps(resp)))
         return {
             "status": job_resp.get("status"),
-            "total": ALL_LANG,
+            "total": list(ALL_LANG),
             "curr_processed": job_resp.get("languages_processed"),
             "pending": lang_to_process,
             "message": "task partially completed"
